bbee28e0b503536a479ba294789727fa33d3481d
kyohei8 <tsukuda.kyouhei@gmail.com>
2015-08-06 14:57:11 +0900

    fixed multiple files to browserify problem.

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -6,33 +6,33 @@ import babelify from 'babelify';
 import through2 from 'through2';
 import {stream as wiredep} from 'wiredep';
 import handleError from '../utils/handleError';
+import path from 'path';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 const withSourceMap = true;
-const destDir = '.tmp/scripts';
 
 gulp.task('js', ['lint'], () => {
+  $.util.log(`Starting '${$.util.colors.cyan('Browserify')}'`);
   gulp.src('app/scripts/*.js')
   .pipe($.plumber())
-  .pipe(destDir)
   .pipe(through2.obj( (file, enc, next) => {
     browserify(file.path, { debug: withSourceMap })
     .transform(babelify)
     .bundle((err, res) => {
+      const fileName = path.basename(file.path);
+      const msg = `${$.util.colors.magenta(fileName)} is Browserified.` ;
       if (err) { return next(err); }
+      $.util.log(msg);
       file.contents = res;
       next(null, file);
     });
   }).on('finish', function(){
-    $.util.log(`Finished '${$.util.colors.blue('Browserify')}'`);
+    $.util.log(`Finished '${$.util.colors.cyan('Browserify')}'`);
+    reload();
   }))
   .on('error', handleError)
-  // .pipe($.sourcemaps.init())
-  //.pipe($.concat('app.js'))
-  // .pipe($.babel())
-  // .pipe($.sourcemaps.write())
-  .pipe(gulp.dest(destDir))
-  .pipe(reload({stream: true}));
+  .pipe(gulp.dest('.tmp/scripts'))
 
 });
+
