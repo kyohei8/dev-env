d894e314342644329082a54c2b2c03afeee24b2a
kyohei8 <tsukuda.kyouhei@gmail.com>
2016-05-03 11:10:13 +0900

    remove extra task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,6 @@ import './gulp/tasks/html';
 import './gulp/tasks/clean';
 import './gulp/tasks/images';
 import './gulp/tasks/fonts';
-import './gulp/tasks/extras';
 import './gulp/tasks/serve';
 import './gulp/tasks/wiredep';
 import './gulp/tasks/sprite';
