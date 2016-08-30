3797827b1bc95d9e6b1b2c17ebacd93cd22acad6
kyohei8 <tsukuda.kyouhei@gmail.com>
2016-08-30 13:33:22 +0900

    update iroiro

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,6 @@ import './gulp/tasks/styles';
 import './gulp/tasks/html';
 import './gulp/tasks/images';
 import './gulp/tasks/fonts';
-import './gulp/tasks/serve';
+// import './gulp/tasks/serve';
 import './gulp/tasks/js';
 import './gulp/tasks/build';
